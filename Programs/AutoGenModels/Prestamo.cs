985a72dea1f79aa01f0f4cf8e4ee6d7773bbe1db
Quick <enriquerc260@gmail.com>
2023-05-25 20:26:57 -0600

    Funcion addPrestamo

--- Programs/AutoGenModels/Prestamo.cs
+++ Programs/AutoGenModels/Prestamo.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using static System.Console;
 
 namespace Banco;
 
@@ -61,4 +63,67 @@ public partial class Prestamo
     [ForeignKey("Usuario")]
     [InverseProperty("Prestamos")]
     public virtual Cliente? UsuarioNavigation { get; set; }
+
+    public static (int affected, long folio) 
+    addPrestamo(long empleado, long tipo, string fecSolicitud, decimal pagoMensual, 
+    string fecSiguientePago, long usuario, long estado, long pagosTotales, string fecLiquidacion)
+    {
+        using (Bank db = new())
+        {
+            if (db.Prestamos is null) return (0, 0);
+            DateOnly fecha;
+            if(!DateOnly.TryParse(fecSolicitud, out fecha)) return (0,0);
+            if(!DateOnly.TryParse(fecSiguientePago, out fecha)) return (0,0);
+            if(!DateOnly.TryParse(fecLiquidacion, out fecha)) return (0,0);
+
+            Prestamo p = new()
+            {
+                Empleado = empleado,
+                Tipo = tipo,
+                FecSolicitud = fecSolicitud,
+                FecAprovado = null,
+                //PagoMensual = pagoMensual, 
+                FecSiguientePago = fecSiguientePago, 
+                Usuario = usuario,
+                Estado = estado, 
+                PagosTotales = pagosTotales,
+                FecLiquidacion = fecLiquidacion
+            };
+            
+            EntityEntry<Prestamo> entity = db.Prestamos.Add(p);
+            int affected;
+            try{
+                affected = db.SaveChanges();
+                }catch(Microsoft.EntityFrameworkCore.DbUpdateException e){
+                    WriteLine($"{e}");
+                    affected = 0;
+                }
+            return (affected, p.Folio);
+        }
+    }
+
+    public static void ListPrestamos(int? []? IdToHighlight = null)
+    {
+        using(Bank db = new())
+        {
+            if((db.Prestamos is null) || (!db.Prestamos.Any()))
+            {
+                WriteLine("There are no loans");
+                return;
+            }
+            WriteLine("| {0,-6} | {1,-12} | {2,12} | {3,5} | {4, 5} | {5, 5} | {6, 12} | {7, 10} | {8, 5} | {9, 5} | {10, 5} |",
+            "Folio", "Empleado", "Tipo", "FecSolicitud", "fecAprovada", "pagoMensual", "fecSiguientePago", "usuario", "Estado", "pagosTotales", "fecLiquidacion");
+            foreach (Prestamo p in db.Prestamos)
+            {
+                ConsoleColor previousColor = ForegroundColor;
+                if((IdToHighlight is not null) && (IdToHighlight.Contains((int)c.Nomina)))
+                {
+                    ForegroundColor = ConsoleColor.Green;
+                }
+                WriteLine("| {0,-6} | {1,-12} | {2,12} | {3,5} | {4, 5} | {5, 5} | {6, 12} | {7, 10} | {8, 5} | {9, 5} | {10, 5} |",
+                p.Folio, p.Empleado, p.Tipo, p.FecSolicitud, p.FecAprovado, p.PagoMensual, p.FecSiguientePago, p.Usuario, p.Estado, p.PagosTotales, p.FecLiquidacion);
+                ForegroundColor = previousColor;
+            }
+        }
+    }
 }
